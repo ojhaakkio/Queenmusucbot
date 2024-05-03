cf390fa202b2ad9c51d5075fb77358b375039595
Danishzain05 <163726214+Danishzain05@users.noreply.github.com>
2024-05-03 13:30:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Danishzain05/VIP-MUSIC/blob/master/config.py",
+    "https://github.com/Danishzain05/VIP-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
