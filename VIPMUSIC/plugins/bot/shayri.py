10bba8e57b2eccd7df0a3ea7ae0c24380d1a1f11
Danishzain05 <163726214+Danishzain05@users.noreply.github.com>
2024-05-03 13:42:40 +0530

    Update shayri.py

--- VIPMUSIC/plugins/bot/shayri.py
+++ VIPMUSIC/plugins/bot/shayri.py
@@ -74,9 +74,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_FRIENDSS"),
+                        "❤️SUPPORT❤️", url=f"https://t.me/StatusWorld_05"),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VIP_CREATORS")
+                        "🥺OWNER🥺", url=f"https://t.me/XdLover_05")
                     
                 ]
             ]
@@ -94,9 +94,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_FRIENDSS"),
+                        "❤️SUPPORT❤️", url=f"https://t.me/StatusWorld_05"),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VIP_CREATORS")
+                        "🥺OWNER🥺", url=f"https://t.me/XdLover_05")
                     
                 ]
             ]
